52d94de0bd0b217da1722bd634197d61c530261d
Elda <bme18388030@uniwa.gr>
2023-03-29 20:21:05 +0300

    basic models

--- medisp_project/settings.py
+++ medisp_project/settings.py
@@ -11,10 +11,14 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+STORAGE_FOLDER = "medisp_storage"
+MEDIA_ROOT = os.path.join(BASE_DIR, STORAGE_FOLDER)
+HIST_IMAGES = os.path.join(MEDIA_ROOT, "hist_images")
+os.makedirs(HIST_IMAGES, exist_ok=True)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
