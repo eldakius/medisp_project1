ad99f45c076fcb976b8817600bdb6864d948abe9
eldakius <127986767+eldakius@users.noreply.github.com>
2023-04-30 19:44:17 +0300

    Update views.py

--- machine_learning/views.py
+++ machine_learning/views.py
@@ -25,3 +25,8 @@ class GroupViewSet(viewsets.ModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+
+class LabelModelViewset(viewsets.ModelViewSet):
+queryset = Label.objects.all()
+serializer_class = LabelSerializer
