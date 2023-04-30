609bacb713fe0868be814a50fa38f4e5a1b4abb6
eldakius <127986767+eldakius@users.noreply.github.com>
2023-04-30 21:10:05 +0300

    Update urls.py

--- medisp_project/urls.py
+++ medisp_project/urls.py
@@ -22,6 +22,9 @@ router = routers.DefaultRouter()
 router.register(r"users", views.UserViewSet)
 router.register(r"groups", views.GroupViewSet)
 router.register(r"labels", views.LabelModelViewset)
+router.register(r"histimage", views.HistImageModelViewset)
+
+
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
