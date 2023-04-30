d0bc647e5c3d62d9b2159444d99c525e84274209
eldakius <127986767+eldakius@users.noreply.github.com>
2023-04-30 19:45:54 +0300

    Update urls.py

--- medisp_project/urls.py
+++ medisp_project/urls.py
@@ -21,6 +21,7 @@ from machine_learning import views
 router = routers.DefaultRouter()
 router.register(r"users", views.UserViewSet)
 router.register(r"groups", views.GroupViewSet)
+router.register(r"labels", views.LabelModelViewset)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
