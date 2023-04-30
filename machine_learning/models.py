4eafdbb5565b25fa70414bd8d3e3b0414107b091
eldakius <127986767+eldakius@users.noreply.github.com>
2023-04-30 20:43:56 +0300

    Update models.py

--- machine_learning/models.py
+++ machine_learning/models.py
@@ -1,3 +1,10 @@
-# from django.db import models
+from django.db import models
 
-# Create your models here.
+class Label(models.Model):
+id = models.AutoField(primary_key=True)
+name = models.CharField(unique=True, max_length=120)
+
+class Image(models.Model):
+id = models.AutoField(primary_key=True)
+label = models.ForeignKey(Label, on_delete=models.SET_NULL, related_name="rois", null=True)
+file = models.FileField(upload_to="hist_images", max_length=255
